1a7bcb7d5034970a047eaf748b62ea2cf16b52bf
João Polezi <joao.francisco12123@gmail.com>
2025-11-12 22:07:42 -0300

    Added netlify.toml

--- frontend/borabico/src/components/reset/Scroll.js
+++ frontend/borabico/src/components/reset/Scroll.js
@@ -5,12 +5,12 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "instant", 
-    });
-  }, [pathname]); 
+    if (pathname === "/vagas") {
+      return;
+    }
+
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   return null;
 }
